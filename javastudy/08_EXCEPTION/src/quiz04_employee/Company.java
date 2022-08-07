2789fc0e4b88928637f83e3ff3d1f75343a0fb90
hlheo1011 <hoh1011@naver.com>
2022-08-07 22:57:41 +0900

    2022-08-07

---
+++ javastudy/08_EXCEPTION/src/quiz04_employee/Company.java
@@ -0,0 +1,20 @@
+package quiz04_employee;
+
+import java.util.Scanner;
+
+public class Company {
+	
+	private Employee[] employees;
+	private int idx;
+	private Scanner sc;
+	
+	public Company() {
+		employees = new Employee[5];
+		sc = new Scanner(System.in);
+	}
+	
+	public void addEmployee() throws EmployeeException{
+		
+	}
+
+}
---
+++ javastudy/08_EXCEPTION/src/quiz04_employee/Temporary.java
@@ -0,0 +1,5 @@
+package quiz04_employee;
+
+public class Temporary {
+
+}
